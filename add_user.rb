46939001b0fdbab2a765617d931bcc52692ec0c8
shimesaba-type0 <t.k.shimesaba@gmail.com>
2017-08-30 08:42:04 +0900

    create add_user.rb

--- add_user.rb
+++ add_user.rb
@@ -1,5 +1,6 @@
 # coding: utf-8
 require "faraday"
+require "faraday_middleware"
 require "json"
 require "pp"
 
@@ -7,12 +8,25 @@ require "pp"
 base_url = "http://localhost"
 port = "3000"
 url = "#{base_url}:#{port}"
+
 conn = Faraday.new(:url => url) do |faraday|
- faraday.request  :url_encoded             # form-encode POST params
- #faraday.response :logger                  # log requests to STDOUT
- faraday.headers['Content-Type'] = 'application/json'		# Set Content-Type
- faraday.adapter  Faraday.default_adapter  # make requests with Net::HTTP
+	faraday.request :json
+	faraday.adapter Faraday.default_adapter
 end
+
+
+## sample user
+# ('a'..'z').to_a.sample(5).join
+sample_user = ('a'..'z').to_a.sample(5).join
+age = (1..8).to_a.sample(2).join
+s_user_info = {
+	user: {
+		name: "#{sample_user}",
+		age: age,
+		description: "#{sample_user} description"
+	}
+}
+p s_user_info
 
 ### POST ###
 # add user
@@ -20,8 +34,12 @@ puts "-----------------------------"
 puts " Add user"
 puts "-----------------------------"
 
-res = conn.post "/users", {:name => "David", :age => 33, :description => "new type"}
+#res = conn.post "/users", {:name => "David", :age => 33, :description => "new type"}
+res = conn.post "/users", s_user_info
+
 body = JSON.parse res.body
+#req_headers = JSON.parse res.request_headers
+#pp req_headers
 pp body
 
 

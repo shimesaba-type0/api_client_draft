3ae17cff6a229c5cd265b2ed44eed80b8e600a44
shimesaba-type0 <t.k.shimesaba@gmail.com>
2017-09-01 08:22:41 +0900

    split get_user_info.rb

--- get_user_info.rb
+++ get_user_info.rb
@@ -3,10 +3,13 @@ require "faraday"
 require "json"
 require "pp"
 
-# connection
+### connection
+# make url
 base_url = "http://localhost"
 port = "3000"
 url = "#{base_url}:#{port}"
+
+# connection
 conn = Faraday.new(:url => url) do |faraday|
  faraday.request  :url_encoded             # form-encode POST params
  #faraday.response :logger                  # log requests to STDOUT
@@ -14,22 +17,11 @@ conn = Faraday.new(:url => url) do |faraday|
  faraday.adapter  Faraday.default_adapter  # make requests with Net::HTTP
 end
 
-### GET ###
-# all users
-puts "-----------------------------"
-puts " Get all users"
-puts "-----------------------------"
-
-res = conn.get "/users"
-body = JSON.parse res.body
-pp body
-
-puts "-----------------------------"
-puts " Get user id=2"
-puts "-----------------------------"
-
+### GET
 # get user info about id 2
-res = conn.get "/users/2"
+user_id = "2"
+resource = "/users/#{user_id}"
+res = conn.get resource
 body = JSON.parse res.body
 pp body
 

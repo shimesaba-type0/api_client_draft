33dc57dfd0e1f1ac2aee712e31be738da99ef149
shimesaba-type0 <t.k.shimesaba@gmail.com>
2017-09-04 08:42:30 +0900

    add optparse test script

---
+++ optparse_test.rb
@@ -0,0 +1,34 @@
+# coding: utf-8
+require 'optparse'
+
+opt = OptionParser.new
+
+# opt.on("-a") {|v| p v}
+# opt.on("-b") {|v| p v}
+# 
+# opt.parse!(ARGV)
+# p ARGV
+
+# ruby sample.rb -a foo bar -b baz
+# => true
+#    true
+#    ["foo", "bar", "baz"]
+
+# parse() の場合、ARGVは変更されない。
+# オプションを取り除いた結果は argv に設定される。
+# argv = opt.parse(ARGV)
+# p argv
+
+
+# -----------
+OPT = {}
+opt.on("-a") {|v| OPT[:a] = v}
+opt.on("-b") {|v| OPT[:b] = v}
+
+opt.parse!(ARGV)
+p ARGV
+p OPT
+
+# ruby optparse_test.rb -a foo bar -b baz
+# ["foo", "bar", "baz"]
+# {:a=>true, :b=>true}

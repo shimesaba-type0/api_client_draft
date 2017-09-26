3e58df7b13f74f4ec8d7f6a5f47f06c130fc0949
shimesaba-type0 <t.k.shimesaba@gmail.com>
2017-09-27 08:33:52 +0900

    add: delete_user.rb

---
+++ delete_user.rb
@@ -0,0 +1,105 @@
+#!/usr/bin/env ruby; coding: utf-8
+require "faraday"
+require "faraday_middleware"
+require "json"
+require "pp"
+require "optparse"
+
+# class BadRequest < StandardError; end # 400 用
+# class NotFound < StandardError; end # 404 用
+# class ServerError < StandardError; end # 500 系と接続エラー用
+# class TimeoutError < StandardError; end # タイムアウト用
+
+# コマンドラインオプション
+opt = OptionParser.new
+opt.banner = "Usage: delete_user.rb -i ID"
+OPT = {}
+
+opt.on("-i", "--id=ID", String, "ID value. (MUST)") {|v|
+  OPT[:id] = v
+}
+opt.on("-h", "--help", "Show this message") {
+  puts opt
+  exit
+}
+
+if ARGV.length <= 0
+  puts opt
+  exit
+end
+
+begin
+  opt.parse!(ARGV)
+rescue OptionParser::InvalidOption => e
+  puts "ERROR: #{e}"
+  exit
+rescue OptionParser::ParseError => e
+  puts "ERROR: #{e}"
+  exit
+end
+
+
+
+
+### connection
+# make url
+base_url = "http://localhost"
+port = "3000"
+url = "#{base_url}:#{port}"
+
+# connection
+conn = Faraday.new(:url => url) do |faraday|
+  faraday.request :json
+  # faraday.request  :url_encoded             # form-encode POST params
+  # faraday.response :logger                  # log requests to STDOUT
+  # faraday.headers['Content-Type'] = 'application/json'    # Set Content-Type
+  faraday.adapter  Faraday.default_adapter  # make requests with Net::HTTP
+end
+
+
+### DELETE
+resource = "/users/#{OPT[:id]}"
+res = conn.delete resource
+
+if res.success?
+  #body = JSON.parse res.body
+  puts "Request URL: #{res.env[:url]}"
+  puts "Method: #{res.env[:method]}"
+  puts "Status: #{res.env[:status]}"
+  puts "Reason_phrase: #{res.env[:reason_phrase]}"
+  pp "Response_headers: #{res.env[:response_headers].to_json}"
+else
+  message = {}
+  res.env.each do |k, v|
+    if ["url", "method", "status", "request", "request_headers",
+        "reason_phrase", "response_headers"].include?(k.to_s) then
+      message[k.to_s] = v.to_s
+    end
+  end
+  puts message
+end
+
+
+### Actually, I want to write like below.
+# begin
+# 	res = conn.get resource
+# 	body = JSON.parse res.body
+# 	pp body
+# 
+# 	case res.status
+# 	when 400
+# 		raise BadRequest, res.body # body に十分な情報が入っている物とする
+# 	when 404
+# 		raise NotFound, res.body
+# 	when 500..599
+# 		raise ServerError, res.body
+# 	end
+# rescue Faraday::Error::TimeoutError => e
+# 	raise Timeout, e.message
+# rescue Faraday::Error::ClientError => e	# ConnectionFailed でもいいが、親クラスである ClientError で全部拾ってしまう
+# 	raise ServerError, e.message	# 500系と混ざってしまうので、もうちょい情報増やすか例外分けてもいいかも
+# end
+
+
+
+
